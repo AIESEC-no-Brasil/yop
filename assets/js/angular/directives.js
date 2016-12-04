c33eafbcc5f0d7d74bd975acae2c5af5d8615104
luancorumba <luan@corumba.net>
2016-12-04 00:24:05 -0200

    sliders working

--- assets/js/angular/directives.js
+++ assets/js/angular/directives.js
@@ -13,8 +13,38 @@ function slider() {
     link: function(scope, element, attrs, fn) {
       var $ = jQuery.noConflict();
       $('.slich_slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
     }
   }
